93e5a0d0efd08c607da9f8f4418e1b0b013eb612
Farnooshmo <91014204+Farnooshmo@users.noreply.github.com>
2024-08-07 23:17:09 +0100

    FizzBuzz

---
+++ index.js
@@ -0,0 +1,16 @@
+function fizzBuzz(n) {
+  let arr = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      arr.push("fizzBuzz");
+    } else if (i % 3 === 0) {
+      arr.push("fizz");
+    } else if (i % 5 === 0) {
+      arr.push("buzz");
+    } else {
+      arr.push(i.toString());
+    }
+  }
+  return arr;
+}
+fizzBuzz(6);
